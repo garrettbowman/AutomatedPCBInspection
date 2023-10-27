fb315cd728da4fa89cdb00998312c4937521182f
Gary Bowman <garrettbowman96@gmail.com>
2023-10-27 12:44:23 -0400

    dadded while loop

--- main.py
+++ main.py
@@ -24,62 +24,70 @@ Then change to this directory and type:
 
 
 def main():
+
+
     bot = InterbotixManipulatorXS(
         robot_model='vx300',
         group_name='arm',
         gripper_name='gripper'
-    )
 
     if (bot.arm.group_info.num_joints < 5):
         bot.core.get_logger().fatal('This script requires the robot to have at least 5 joints!')
         bot.shutdown()
         sys.exit()
 
+    while(1):
+        input
 
-    joint_positions = [-1.0, 0.5, 0.5, 0, -0.5, 1.57]
-    # pcl = InterbotixPointCloudInterface(node_inf=bot.core)
-    # # get the cluster positions
-    # # sort them from max to min 'x' position w.r.t. the ARM_BASE_FRAME
-    # success, clusters = pcl.get_cluster_positions(
-    #     ref_frame=ARM_BASE_FRAME,
-    #     sort_axis='x',
-    #     reverse=True
-    # )
-
-    # if success:
-    #     # pick up all the objects and drop them in a virtual basket in front of the robot
-    #     for cluster in clusters:
-    #         x, y, z = cluster['position']
-    #         print(x, y, z)
-    #         bot.arm.set_ee_pose_components(x=x, y=y, z=z+0.05, pitch=0.5)
-    #         bot.arm.set_ee_pose_components(x=x, y=y, z=z, pitch=0.5)
-    #         bot.gripper.grasp()
-    #         bot.arm.set_ee_pose_components(x=x, y=y, z=z+0.05, pitch=0.5)
-    #         bot.arm.set_ee_pose_components(x=0.3, z=0.2)
-    #         bot.gripper.release()
-    # else:
-    #     print('Could not get cluster positions.')
+        )
 
 
-    #bot.arm.set_joint_positions(joint_positions)
 
-    bot.arm.set_ee_pose_components(x=0.3, z=0.2)
-    bot.arm.set_single_joint_position(joint_name='waist', position=np.pi/2.0)
-    bot.gripper.release()
-    bot.arm.set_ee_cartesian_trajectory(x=0.1, z=-0.16)
-    bot.gripper.grasp()
-    bot.arm.set_ee_cartesian_trajectory(x=-0.1, z=0.16)
-    bot.arm.set_single_joint_position(joint_name='waist', position=-np.pi/2.0)
-    bot.arm.set_ee_cartesian_trajectory(pitch=1.5)
-    bot.arm.set_ee_cartesian_trajectory(pitch=-1.5)
-    bot.arm.set_single_joint_position(joint_name='waist', position=np.pi/2.0)
-    bot.arm.set_ee_cartesian_trajectory(x=0.1, z=-0.16)
-    bot.gripper.release()
-    bot.arm.set_ee_cartesian_trajectory(x=-0.1, z=0.16)
-    bot.arm.go_to_home_pose()
-    bot.arm.go_to_sleep_pose()
 
-    bot.shutdown()
+        joint_positions = [-1.0, 0.5, 0.5, 0, -0.5, 1.57]
+        # pcl = InterbotixPointCloudInterface(node_inf=bot.core)
+        # # get the cluster positions
+        # # sort them from max to min 'x' position w.r.t. the ARM_BASE_FRAME
+        # success, clusters = pcl.get_cluster_positions(
+        #     ref_frame=ARM_BASE_FRAME,
+        #     sort_axis='x',
+        #     reverse=True
+        # )
+
+        # if success:
+        #     # pick up all the objects and drop them in a virtual basket in front of the robot
+        #     for cluster in clusters:
+        #         x, y, z = cluster['position']
+        #         print(x, y, z)
+        #         bot.arm.set_ee_pose_components(x=x, y=y, z=z+0.05, pitch=0.5)
+        #         bot.arm.set_ee_pose_components(x=x, y=y, z=z, pitch=0.5)
+        #         bot.gripper.grasp()
+        #         bot.arm.set_ee_pose_components(x=x, y=y, z=z+0.05, pitch=0.5)
+        #         bot.arm.set_ee_pose_components(x=0.3, z=0.2)
+        #         bot.gripper.release()
+        # else:
+        #     print('Could not get cluster positions.')
+
+
+        #bot.arm.set_joint_positions(joint_positions)
+
+        bot.arm.set_ee_pose_components(x=0.3, z=0.2)
+        bot.arm.set_single_joint_position(joint_name='waist', position=np.pi/2.0)
+        bot.gripper.release()
+        bot.arm.set_ee_cartesian_trajectory(x=0.1, z=-0.16)
+        bot.gripper.grasp()
+        bot.arm.set_ee_cartesian_trajectory(x=-0.1, z=0.16)
+        bot.arm.set_single_joint_position(joint_name='waist', position=-np.pi/2.0)
+        bot.arm.set_ee_cartesian_trajectory(pitch=1.5)
+        bot.arm.set_ee_cartesian_trajectory(pitch=-1.5)
+        bot.arm.set_single_joint_position(joint_name='waist', position=np.pi/2.0)
+        bot.arm.set_ee_cartesian_trajectory(x=0.1, z=-0.16)
+        bot.gripper.release()
+        bot.arm.set_ee_cartesian_trajectory(x=-0.1, z=0.16)
+        bot.arm.go_to_home_pose()
+        bot.arm.go_to_sleep_pose()
+
+        bot.shutdown()
 
 
 if __name__ == '__main__':
